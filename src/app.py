32e88bd1172c6d75de49ecb7edf80be88869ba18
Julian Amirian <jamirian0714@gmail.com>
2020-05-26 12:42:53 -0700

    Fixed inconsistent transcript results

--- src/app.py
+++ src/app.py
@@ -4,6 +4,20 @@ from youtube_transcript_api import YouTubeTranscriptApi
 def lambda_handler(event, context):
     print(event)
 
+    try:
+        transcript_data = YouTubeTranscriptApi.get_transcript(vid_id)
+    except:
+        response = {
+            'statusCode': 200,
+            'headers': { 
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({"no_captions": "true"})
+        }
+
+        print(response)
+        return response
+
     query_lower = event['queryStringParameters']['query'].lower()
     query_upper = ''
 
@@ -19,23 +33,9 @@ def lambda_handler(event, context):
 
     timestamps = []
 
-    try:
-        transcript_data = YouTubeTranscriptApi.get_transcript(vid_id)
-        
-        for caption in transcript_data:
-            if 'text' in caption and 'start' in caption and (query_lower in caption['text'] or query_upper in caption['text']):
-                timestamps.append(caption['start'])
-    except:
-        response = {
-            'statusCode': 200,
-            'headers': { 
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': json.dumps({"no_captions": "true"})
-        }
-
-        print(response)
-        return response
+    for caption in transcript_data:
+        if 'text' in caption and 'start' in caption and (query_lower in caption['text'] or query_upper in caption['text']):
+            timestamps.append(caption['start'])
 
     response = {
         'statusCode': 200,
